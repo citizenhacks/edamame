9deadf51e4f475d06c2235ad58152bca044ae746
Daniel Wu <wuon@protonmail.com>
2019-06-16 17:15:19 -0400

    added favicon

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'Citizen Hacks',
+    description: 'For 36 hours, join students and leading innovators in tackling the challenge of privacy in the digital age.',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
+    lastUpdated: 'Last Updated', // string | boolean
+}
